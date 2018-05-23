b9999c126f2b10118be6290a27e7bd37a46a29e3
Ttommeke <tomvalkeneershotmail.com>
2018-05-23 15:22:25 +0200

    nummer 2

--- openFilesFromDir.m
+++ openFilesFromDir.m
@@ -1,11 +1,11 @@
 %% load in data first
 function [allSamples] = openFilesFromDir(subdir)
-    files= dir(strcat('.\HMP_Dataset\',subdir,'\A*.txt'));
-    L = length(files); %% number of files to read
-    allSamples{L}=[]; %% create empty 1 x L cell Array
+    files= dir(strcat('./HMP_Dataset/',subdir,'/A*.txt'));
+    L = length(files) %% number of files to read
+    allSamples = {L}; %% create empty 1 x L cell Array
 
     for i = 1:L
-        data = textread([strcat('.\HMP_Dataset\', subdir, '\'), files(i).name]);
+        data = textread([strcat('./HMP_Dataset/', subdir, '/'), files(i).name]);
         allSamples{i}=dataToRealValues(data); % !!! -> {} to access values inside cell arrays
     end
 end
