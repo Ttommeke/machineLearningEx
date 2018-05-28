48d9bf3276dd17653c907a66a8a9cd2be1570ce8
ArguShards <chiel.dommange@gmail.com>
2018-05-28 11:29:50 +0200

    added documentation for task division

--- taak.m
+++ taak.m
@@ -2,11 +2,25 @@
 
 %first step load data
 data= openFilesFromDir('Drink_glass');
+%% exercise 1
+
+%1.1: preprocessing
 [training,validation,testing]= divideAndConquer(data);
 
+%1.2: feature extraction
+Gravity = getGravity(training{1}); %1.2.1
+ampdata = GetAmplitudeOfAcceleration(training{1}); %1.2.2
+nodc = RemoveDCComponent(training{1}); %1.2.3
+[stdev,skew] = timeDomainFeatures(training{1}); %1.2.4
+[f25, f75] = getFrequencyDomain(training{1}); %1.2.5
 
-Gravity = getGravity(training{1});
-[f25, f75] = getFrequencyDomain(training{1});
-[stdev,skew] = timeDomainFeatures(training{1});
+%1.3 feature selection: TODO
 
-nodc=RemoveDCComponent(training{1});
+%% exercise 2 
+%2.1 cost function and gradient
+%2.2 linear model with 2 features
+%2.3 polynomial features from 2 features
+%2.4 linear classifier with 7 features
+%2.5 non-linear classifier with 7 features
+
+
